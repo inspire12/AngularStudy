b67aa507d2a50f81bf201002c177c8d9df6d8990
Your Name <you@example.com>
2018-05-27 16:27:56 +0900

    selected 추가

--- src/app/selected/selected.component.ts
+++ src/app/selected/selected.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// assets/data/book.json
+interface IBook {
+  bauthor: string;
+  bdate: string;
+  btranslator: string;
+  bpublisher: string;
+  btitle: string;
+  bprice: number;
+  bisbn: string;
+  bimgurl: string;
+}
 
 @Component({
   selector: 'app-selected',
@@ -6,10 +19,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selected.component.css']
 })
 export class SelectedComponent implements OnInit {
+  books: IBook[];
+  listOption: string[];
+  constructor() {
 
-  constructor() { }
+  }
+  // constructor(private http: HttpClient) {
+  //   this.http.get< IBook[] > ('assets/data/book.json').subscribe(res => this.books = res);
+  // }
 
   ngOnInit() {
+    this.listOption = []
+    this.listOption.push('카일 루든(Kyle Loudon)');
+    this.listOption.push('권기경, 박용우');
   }
 
 }
--- src/app/selected/selected.module.ts
+++ src/app/selected/selected.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectedComponent } from './selected.component';
+import {MatFormFieldModule, MatOptionModule, MatSelectModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+
   ],
   declarations: [SelectedComponent],
   exports: [SelectedComponent]
